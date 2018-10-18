a6baa56719514d4264c8843ecaf5ab27b220d655
Mustapha <ait.mus06@gmail.com>
2018-10-18 19:15:45 +0200

    ajout du programme mailer + html

--- lib/app/townhall_mailer.rb
+++ lib/app/townhall_mailer.rb
@@ -6,62 +6,48 @@ require 'googleauth/stores/file_token_store'
 require 'fileutils'
 require 'json'
 require 'csv'
-
+require_relative 'JsonInterface'
+require 'dotenv'
+Dotenv.load("../../.env")
+#Nous initialisons les différentes gem qui votn nous servir tout au long de la class mailer
 class Mailer
 
-  attr_accessor :contacts
-
-  def Initialize(file)
-    @text_mail_to_send
-    @contacts = []
-
-    #recup_mail_from_csv_json
-
+  def initialize
+      @contacts = JsonInterface.get_datas_from_json_file("../../db/contacts.json")
+    # On récupère les contacts dans le fichier contacts.json auxquelles on va envoyer le mail?
+      @adresse_mail = ENV['ADRESSE_MAIL']
+      @mdp = ENV['MDP']
   end
 
+# Cette méthode nous permet de se connecter au compte Gmail de THP Lille et d'envoyer un mail aux adresses mails présentes dans le fichier contacts.json
+  def send_mail_to_townhall(contact)
 
-
-  def recup_mail_from_csv_json
-    # On va avoir besoin du fichier csv ou JSON. Tous les contacts dans ce fichier vont être stockées
-    # dans la variable @contacts. Puis on va boucler sur la variable @contacts pour créer notre spreadsheet.
-    session= GoogleDrive::Session.from_config("config.json")
-    ws = session.spreadsheet_by_key("").worksheet(0)
-    ws [1, 1] = "Nom de la ville"
-    ws [1, 2] = "Mail de la ville"
-    ws [1, 3] = "Numéro de département"
-
-    row = 2
-    @contacts.each do |contact|
-      ws [1, 1] = contact["name"]
-      ws [1, 2] = contact["mail_ville"]
-      ws [1, 3] = contact["dept_ville"]
-    row = row + 1
-    end
-ws.save
-end
-
-
-
-def send_mail_to_townhall
-
-  gmail = Gmail.connect!("thehackingproject.lille@gmail.com","teamwelsh")
-    #On va utiliser la spreadsheet crée auparavant pour recuperer les mails. On va envoyer un mail contenu dans un
-    #fichier HTML à chaque mail récupérés.
-
-    gmail.deliver do
-    to "ait.mus06@gmail.com"
-    subject " objet"
-      text_part do
-      body "test mail"
-      end
+    gmail = Gmail.connect!(@adresse_mail,@mdp)
+      gmail.deliver do
+        to  contact["email"]
+        subject " Présentation de The Hacking Project "
       html_part do
-      content_type 'text/html; charset=UTF-8'
-      body " Test mail"
+        content_type 'text/html; charset=UTF-8'
+         body
+          "Bonjour,</br>
+               Nous sommes élèves à The Hacking Project, une formation au code gratuite, sans locaux, sans sélection, sans restriction géographique. La pédagogie de ntore école est celle du peer-learning, où nous travaillons par petits groupes sur des projets concrets qui font apprendre le code. Le projet du jour est d'envoyer (avec du codage) des emails aux mairies pour qu'ils nous aident à faire de The Hacking Project un nouveau format d'éducation pour tous.</br>
+               Déjà 500 personnes sont passées par The Hacking Project. Est-ce que vous voulez changer le monde avec nous ?</br>
+               Charles, co-fondateur de The Hacking Project pourra répondre à toutes vos questions : 06.95.46.60.80</br>
+               Excellente journée à vous,</br>
+               La team Lille de The Hacking Project </br>"
+      end
       end
   end
 
-end
+  def send_emails
+# cette méthode nous permet d'envoyer les mails en utilisant la méthode send_mail_to_townhall et le fichier contacts.json
+    @contacts.each do |contact|
+      send_mail_to_townhall(contact)
+      puts "Email envoyé à #{contact['name']}"
+      sleep 0.1
+    end
+  end
 end
 
 spammer = Mailer.new
-spammer.send_mail_to_townhall
+spammer.send_emails
