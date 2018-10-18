76a158a9ddc77317e99261bb9c48e9ec51bf32d8
StarrK <tony.kino.3@gmail.com>
2018-10-19 00:07:59 +0200

    Add twitter

--- lib/app/townhall_follower.rb
+++ lib/app/townhall_follower.rb
@@ -5,48 +5,67 @@ require_relative 'db_interface'
 
 Dotenv.load
 
-class Follow
-  attr_accessor :client
-  def initialize
+class TownhallTwitter
+  def initialize # A l'initialisation, on créé le client twitter
     @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV["config_key"]
-      config.consumer_secret     = ENV["config_secret"]
-      config.access_token        = ENV["token_key"]
-      config.access_token_secret = ENV["token_secret"]
+      config.consumer_key        = ENV["THPLILLE_CONFIG_KEY"]
+      config.consumer_secret     = ENV["THPLILLE_CONFIG_SECRET"]
+      config.access_token        = ENV["THPLILLE_TOKEN_KEY"]
+      config.access_token_secret = ENV["THPLILLE_TOKEN_SECRET"]
     end
+    # Et on récupère la liste des contacts
     @contacts = JsonInterface.get_datas_from_json_file('db/contacts.json')
   end
 
-  def letsgo
-    update_contacts
-    # all_json
-    # ideas
-    # following
-  end
-  
-  def update_contacts
-    # pour chaque contact, j'ajoute une section handle
-    @contacts.each do |contact|
-      contact["twitter"] = get_handle(contact['name'])
+  def update_contacts_in_json
+    @contacts.each do |contact| # POur chaque contact (hash)
+      handle = get_handle(contact['name']) if contact # Je récupère le @handle (le if est préventif au cas ou un contact est nil)
+      contact["twitter"] = handle if handle # Si le handle existe (si la mairie a un compte twitter), je l'ajoute aux infos du contact
     end
-     JsonInterface.save_into_json_file(@contacts, 'db/contacts.json')
+    JsonInterface.save_into_json_file(@contacts, 'db/contacts.json') # Je sauvegarde la liste de contacts modifiés (format json)
   end
+
   def get_handle(town)
-    handle = @client.user_search("mairie #{town['name']}")
+    # Je fais une recherche twitter "mairie + nom de la ville"
+    handle = @client.user_search("mairie #{town}") if town # je récupère un tableau de résultats (if préventif, comme au dessus)
+    if handle != [] # SI le tableau n'est pas vide (j'ai trouvé des comptes avec ma recherche)
+      return handle[0].screen_name # Je return le @screen_name (le nom visible du compte qui commence par @) du 1er résultat
+    end
   end
-  #
-  # def follow_handle
-  #   @contacts.each do |contact|
-  #     client.follow[contact["twitter"]]
-  #   end
-  # end
-  #
-  # def ideas
-  #   @tab.each do |town_hall|
-  #     @townhall_id = @client.user_search(town_hall)
-  #       @townhall_id.each do |id|
-  #       client.follow(id)
-  #       end
-  #   end
-  # end
+
+  def how_many_townhall_use_twitter
+    counter = 0
+    @contacts.each do |contact| # Pour chaque contact
+      if contact != nil # if préventif
+        counter += contact.keys.count do |k| # Je compte le nombre de mes contacts ayant un compte twitter
+          k.include?('twitter') # En regardant simplement si la clé "twitter" existe dans la fiche du contact
+        end
+      end
+    end
+    counter # Je return counter
+  end
+
+  def print_contacts_if_twitter
+    # J'affiche tous les contacts possédant un compte twitter
+    @contacts.each do |contact|
+      puts contact if contact != nil && contact.keys.include?('twitter')
+    end
+  end
+
+  def follow_twitter_accounts
+    # Je créé un array des comptes que je souhaite follow
+    to_follow = []
+    @contacts.each do |contact| # Pour chaque contact
+      if contact != nil && contact.keys.include?('twitter') # S'il a un compte twitter
+        to_follow << client.user(contact['twitter']) # Je récupère son id user à partir du screen_name et je l'ajoute au array
+      end
+    end
+    client.follow(to_follow) # Je follow tous les users de mon array
+  end
 end
+
+test = TownhallTwitter.new
+#test.update_contacts_in_json  # Attention, cette ligne va bannir le compte twitter (too many request)
+#p test.how_many_townhall_use_twitter
+#test.print_contacts_if_twitter
+#test.follow_twitter_accounts
