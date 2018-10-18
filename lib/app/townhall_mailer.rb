c6b90742fe5c87588ca36363ee92bf5653ced0c5
TheLondonWhale <thomas.perus@espeme.com>
2018-10-18 22:26:27 +0200

    motifs des gems dans tous les fichiers sauf le bot twitter. premier test de launch de app.rb non concluant

--- lib/app/townhall_mailer.rb
+++ lib/app/townhall_mailer.rb
@@ -1,19 +1,17 @@
 require 'gmail'
-require 'google/apis/gmail_v1'
 require 'googleauth'
 require 'gmail_xoauth'
-require 'googleauth/stores/file_token_store'
 require 'fileutils'
 require 'json'
-require 'csv'
-require_relative 'JsonInterface'
+require_relative 'db_interface'
 require 'dotenv'
+
 Dotenv.load("../../.env")
 #Nous initialisons les différentes gem qui votn nous servir tout au long de la class mailer
 class Mailer
 
   def initialize
-      @contacts = JsonInterface.get_datas_from_json_file("../../db/contacts.json")
+      @contacts = JsonInterface.get_datas_from_json_file("db/contacts.json")
     # On récupère les contacts dans le fichier contacts.json auxquelles on va envoyer le mail?
       @adresse_mail = ENV['ADRESSE_MAIL']
       @mdp = ENV['MDP']
